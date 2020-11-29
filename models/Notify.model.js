684e36731654bf9ad1969803ad3393c7acc70742
unknown <CS18B008@iittp.ac.in>
2020-11-29 16:23:19 +0530

    fixed type field

--- models/Notify.model.js
+++ models/Notify.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const notifySchema = new mongoose.Schema({
-    type: {type: String, required: true, enum:["Bid Placed", "Bid Accepted"]},
+    type: {type: String, required: true, enum:["Bid Placed", "Bid Accepted", "Blog Recommendation"]},
     msg: {type: String, required: true},
     url: {type: mongoose.Schema.Types.ObjectId, required: true},
     subject: {type: mongoose.Schema.Types.ObjectId, required: true}
